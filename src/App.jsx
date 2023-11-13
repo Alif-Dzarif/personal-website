948fb444d4eac7449fd30281088e5ddbcb748ff9
Alif-Dzarif <abdulaode9@gmail.com>
2023-11-13 14:18:21 +0700

    chore: add some content on About Me page

--- src/App.jsx
+++ src/App.jsx
@@ -21,43 +21,6 @@ function App() {
   const containerRef = useRef(null);
 
 
-  const handleScroll = () => {
-    // Get the container element
-    const container = containerRef.current;
-    // Get the four elements by name
-    const intro = document.querySelector('[name="intro"]');
-    const project = document.querySelector('[name="project"]');
-    const contribution = document.querySelector('[name="contribution"]');
-    const contact = document.querySelector('[name="contact"]');
-    // Get the viewport width and height
-    const vw = window.innerWidth || document.documentElement.clientWidth;
-    const vh = window.innerHeight || document.documentElement.clientHeight;
-    // Define a function to check if an element is visible in the viewport
-    const isVisible = (element) => {
-      // Get the bounding rectangle of the element
-      const rect = element.getBoundingClientRect();
-      // Check if the element is within the viewport boundaries
-      return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= vh &&
-        rect.right <= vw
-      );
-    };
-    // Loop through the four elements and find the first one that is visible
-    for (let element of [project, intro, contact, contribution]) {
-      // Check if the element is visible
-      if (isVisible(element)) {
-        // Print the element name to the console
-        console.log(element.getAttribute('name'));
-        // Break the loop
-        break;
-      }
-    }
-  };
-
-
-
   const night = () => {
     setBgTime('bg-gradient-midnight');
     setTextColor('text-white');
@@ -120,35 +83,35 @@ function App() {
       setPageSlide('-translate-y-[0rem]')
       scroller.scrollTo('intro', {
         smooth: true,
-        duration: 400
+        duration: 500
       })
     }
     else if (pages === 2) {
       setPageSlide('-translate-y-[6.45rem]')
       scroller.scrollTo('aboutme', {
         smooth: true,
-        duration: 400
+        duration: 500
       })
     }
     else if (pages === 3) {
       setPageSlide('-translate-y-[12.9rem]')
       scroller.scrollTo('project', {
         smooth: true,
-        duration: 400
+        duration: 500
       })
     }
     else if (pages === 4) {
       setPageSlide('-translate-y-[19.35rem]')
       scroller.scrollTo('contribution', {
         smooth: true,
-        duration: 400
+        duration: 500
       })
     }
     else if (pages === 5) {
       setPageSlide('-translate-y-[25.8rem]')
       scroller.scrollTo('contact', {
         smooth: true,
-        duration: 400
+        duration: 500
       })
     }
   }, [pages])
@@ -159,8 +122,8 @@ function App() {
   }, []);
 
   return (
-    <div className={`back-body w-full h-full ${bgTime} bg-fixed ${textColor} theme overflow-hidden`} ref={containerRef}
-      onScroll={handleScroll}
+    <div className={`back-body w-full h-full ${bgTime} bg-fixed ${textColor} theme overflow-hidden`} 
+    
       >
       <div className="fixed h-[120px] py-10 px-20 top-0 left-0 right-0 bg-white bg-opacity-[0.001]">
         <button className={`button button-fill border-2 ${buttonBorder} 
@@ -192,7 +155,7 @@ function App() {
         </div>
       </aside>
       <div className={`top-0 bottom-0 h-2 bg-black w-[]`}></div>
-      <div className='slide-down  h-screen py-44 px-16 flex justify-center select-none' name='intro' ref={containerRef} onScroll={handleScroll}>
+      <div className='slide-down  h-screen py-44 px-16 flex justify-center select-none' name='intro' >
         <div className='mt-36'>
           <h1 className=' text-6xl flex font-bold '>Hello <span className='mr-3 ml-3'><a href="https://emoji.gg/emoji/2084-heya"><img src="https://cdn3.emoji.gg/emojis/2084-heya.gif" width="50px" height="50px" alt="heya" /></a></span> I'm La Ode Abdul Dzarif Imaduddin</h1>
           <h1 className='text-3xl mt-5 font-medium'>I'm a
@@ -211,9 +174,9 @@ function App() {
         </div>
       </div>
       <AboutMe textColor={textColor} />
-      <ProjectCarousel textColor={textColor} className="slide-down" ref={containerRef} onScroll={handleScroll} />
-      <GitContribution textColor={textColor} ref={containerRef} onScroll={handleScroll} />
-      <GetInTouch ref={containerRef} onScroll={handleScroll} />
+      <ProjectCarousel textColor={textColor} className="slide-down"  />
+      <GitContribution textColor={textColor}  />
+      <GetInTouch  />
     </div>
   )
 }
--- src/components/GitContribution.jsx
+++ src/components/GitContribution.jsx
@@ -4,7 +4,7 @@ export default function GitContribution({ textColor }) {
   return (
     <div className='h-screen w-screen px-40 pt-32 pb-20' name='contribution'>
       <h1 className={`text-center text-5xl font-bold ${textColor}`}>GITHUB CONTRIBUTION</h1>
-      <div className='px-52 mt-20'>
+      <div className='px-52 mt-24'>
         <h1 className={`text-2xl font-semibold ${textColor}`}>Github Username : Alif-Dzarif</h1>
       </div>
       <div className='w-full flex justify-center mt-10'>
